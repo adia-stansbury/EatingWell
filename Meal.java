b4a1f37bca5212da21279e11b6e313e2a383d9f7
Adia Porter <adia-stansbury@users.noreply.github.com>
2020-04-16 15:57:31 -0400

    Hardcodes amount of nutrient in a food
    Adds food nutrient map with nutrient details for a food

--- Meal.java
+++ Meal.java
@@ -6,19 +6,28 @@
 package adia.nutritionalreportapp;
 import java.util.EnumMap;
 import java.util.HashMap;
+import java.util.AbstractMap;
 
 /**
  *
  * @author Adia
  */
 public class Meal {
-    EnumMap<Food, HashMap<String, Double>> foods;
-    HashMap<String, Double> foodMealDetails = new HashMap();
-    
+    private static final double GRAM_PORTION_WEIGHT = 100.00;
+    HashMap<String, AbstractMap> foods = new HashMap();
+    EnumMap<Nutrient, HashMap> foodNutrientDetails = new EnumMap<Nutrient, HashMap>(Nutrient.class);
+   
     Meal() { 
-        foods = new EnumMap<Food, HashMap<String, Double>>(Food.class);
-        foodMealDetails.put("gramWeight", 245.00);
-        foods.put(Food.YOGURT_GREEK_WHOLE_MILK_PLAIN, foodMealDetails);
+        HashMap<String, Double> foodDetails = new HashMap();
+        foodDetails.put("gramWeight", 245.00);
+        foods.put("Yogurt,Greek,Whole Milk,Plain", foodDetails);
+        HashMap<String, String> yogurtZincUnitName = new HashMap();
+        yogurtZincUnitName.put("unitName", "mg");
+        HashMap<String, Double> yogurtZincAmount = new HashMap();
+        yogurtZincAmount.put("amount", 0.52);
+        foodNutrientDetails.put(Nutrient.ZINC, yogurtZincUnitName);
+        foodNutrientDetails.put(Nutrient.ZINC, yogurtZincAmount);
+        foods.put("nutrients", foodNutrientDetails);
     }
     
     public double nutrientAmount(Nutrient nutrient) {
@@ -26,11 +35,17 @@ public class Meal {
         return 4;
     }
     
-    private double nutrition(Food food, Nutrient nutrient) {
-        return foodPortion(food) * Food.zincAmount();
+    private double nutrition(String food, Nutrient nutrient) {
+        return gramFoodPortion(food) * gramFoodNutrientAmount(food, nutrient);
     };
     
-    private double foodPortion(Food food) {
-        return foods.get(food).get("gramWeight")/Food.PORTION_AMOUNT;  
+    private double gramFoodPortion(String food) {
+        System.out.println(foods.get("Yogurt,Greek,Whole Milk,Plain"));
+        return foods.get(food).get("gramWeight")/GRAM_PORTION_WEIGHT;  
+    }
+    
+    private double gramFoodNutrientAmount(String food, Nutrient nutrient) {
+        // TODO: convert amount to grams
+        return foods.get(food).get("nutrients").get(nutrient).get("amount");
     }
 }
